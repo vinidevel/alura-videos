6f383b7309a6efacdb7e5e30185f989afbf76981
vinidevel <vini_ide@yahoo.com.br>
2025-05-19 20:50:43 -0300

    first commit

---
+++ novo-video.php
@@ -0,0 +1,25 @@
+<?php
+
+$dbPath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO("sqlite:$dbPath");
+
+$url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
+if ($url === false) {
+    header('Location: /?sucesso=0');
+    exit();
+}
+$titulo = filter_input(INPUT_POST, 'titulo');
+if ($titulo === false) {
+    header('Location: /?sucesso=0');
+    exit();
+}
+
+$repository = new Alura\Mvc\Repository\VideoRepository($pdo);
+$repository->add(new Alura\Mvc\Entity\Video($url, $titulo));
+
+if ($repository->add(new Alura\Mvc\Entity\Video($url, $titulo)) === false) {
+    header('Location: /?sucesso=0');
+} else {
+    header('Location: /?sucesso=1');
+}
+
